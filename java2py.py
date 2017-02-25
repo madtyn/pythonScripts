26ad39366c9de57cc1da7f9a8d01dcbe7056526d
madtyn <madtyn@vant>
2017-02-25 03:11:27 +0100

    Agregados TODOs. Arreglado bug de retorno de carro en definicion de clase. Arreglado bug de constructor __init__. Agregadas algunas expresiones comunes de Java (equals, != null...)

--- java2py.py
+++ java2py.py
@@ -1,4 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
+'''
+TODO Revisar el caso de que sea interface, y si lo es, detectar los metodos de distinta manera
+TODO ArrayList<\b\w*\b> deberia sustituirse por []
+TODO Mejorar procesamiento de args en metodos
+TODO Corregir el problema de los alias en los for(Object x : objects) { 
+TODO bug con las sentencias } else {
+TODO .equals por ' == '
+Ej: line = re.sub('\b\w*\b \b(\w*)\b[,\)]', '\1,', line)
+
+TODO en declaraciones de variables tambien se cumple:
+\b(?P<tipo>\w*)\b \b\w*\b; 
+o bien
+\b(?P<tipo>\w*)\b \b\w*\b = (?P<valor); 
+'''
+
 
 import re
 import sys, os
@@ -13,83 +29,157 @@ def transform(javaFile, pyFile):
 	with open(javaFile, 'r') as jFile:
 		with open(pyFile, 'w+') as pFile:
 			className = None
+			interfaceName = None
 			for oldLine in jFile:
-				line = oldLine
-
+				line = oldLine[:]
+				line.rstrip()
+				
 				match = re.search(r'^(\s*)package', line)
 				if match:
 					line = re.sub(r'^(\s*)package', r'\1#package', line)
+					pFile.write(line)
 					continue
 
 				match = re.search(r'^(\s*)import', line)
 				if match:
 					line = re.sub(r'^(\s*)import', r'\1#import', line)
+					pFile.write(line)
+					continue
+				
+				match  = re.search(r'^\s*}\s*$', line)
+				if match:
+					line = re.sub(r'}', r'', line)
+					pFile.write(line)
 					continue
 
 				# CLASS DECLARATION
 				classDefLine = re.search(r'class (?P<name>\w+)', line)
 				if classDefLine:
 					className = classDefLine.group('name')
-					className = re.compile(className)
-				line = re.sub(r'(.*?)\w* class (?P<name>\w+)(.*){.*(?P<nline>\r?\n?)', r'\1def class \g<name>(): \3\g<nline>', line)
+				line = re.sub(r'(.*?)\w* class (?P<name>\w+)(.*?)\s*{(?P<nline>\r?\n?)', r'\1class \g<name>(): \3\g<nline>', line)
+				
+				interfaceDefLine = re.search(r'interface (?P<name>\w+)', line)
+				if interfaceDefLine:
+					interfaceName = interfaceDefLine.group('name')
+					interfaceName = re.compile(interfaceName)
+				line = re.sub(r'(.*?)\w* interface (?P<name>\w+)(.*?)\s*{(?P<nline>\r?\n?)', r'\1class \g<name>(): \3\g<nline>', line)
 
 				# Adds the parents
-				line = re.sub(r'\(\).*extends (?P<parents>(.*))', r'(\g<parents>):', line)
+				line = re.sub(r'\(\).*extends (?P<parents>([ .\w]*))\s*?(?P<nline>\r?\n?)', r'(\g<parents>):\g<nline>', line)
 
 				# Adds the interfaces if there were no parents
 				line = re.sub(r'\(\).*implements (?P<contracts>(.*))', r'(\g<contracts>)', line)
 				# Adds the interfaces if there were parents
 				line = re.sub(r'\((?P<parents>.+?).*implements (?P<contracts>\S*)\s*', r'(\g<parents>,\g<contracts>', line)
 
+				if not classDefLine and className:
+					pattern='\b'+className
+					line = re.sub(r'\b'+className+r'\b', '__init__', line)
 
-				# Operators
-				line = re.sub(r'!', 'not ', line)
+				# Declaration
+				declaration = False
+				declaration = re.search(r'public|private|protected', line)
+				if declaration:
+					print('DECLARATION: '+line)
+					isFinal = False
+					isStatic = False
+					prefix = ''
+					privacy = ''
+					privacy = declaration.group()
+					if privacy == 'private':
+						prefix = '__'
+					elif privacy == 'protected':
+						prefix = '_'
+					isStatic = re.search(r'static ', line)
+					if isStatic:
+						line = re.sub(r'static ', '', line)
+					isFinal = re.search(r'final ', line)
+					if isFinal:
+						line = re.sub(r'final ', '', line)
+					
+					if re.search(r'=',line):
+						isMethod = False
+						isMethodArgs = False
+					elif not re.search(r';$', line) or interfaceName:
+						isMethod = re.search(r'(.*?)(?P<fname>\w*)\(\) *{', line)
+						isMethodArgs = re.search(r'(.*?)(?P<fname>\w*\()(?P<args>.*\)).+{', line)
+						
+					if isMethod:
+						print('IS_METHOD')
+						line = re.sub(r'(.*?)(?P<fname>\w*)\(\) *{', r'\1def '+prefix+r'\g<fname>(self):', line)
+						line = re.sub(r'\w* def', r'def', line)
+						if isStatic:
+							re.search('(?P<name>\s+)', line).group()
+							line = '@staticmethod\n' + line
+						line = os.linesep + line
+					elif isMethodArgs:
+						print('IS_METHOD')
+						line = re.sub(r'(.*?)(?P<fname>\w*\()(?P<args>.*\)).+{', r'\1def '+prefix+r'\g<fname>self,\g<args>: ', line)
+						line = re.sub('\b\w*\b\s*\b(\w*)\b[,\)]', '\1,', line)
+						line = re.sub(r'\w* def', r'def', line)
+						if isStatic:
+							re.search('(?P<name>\s+)', line).group()
+							line = '@staticmethod\n' + line
+						line = os.linesep + line
+					else:
+						print('IS_VARIABLE')
+						line = re.sub(r'(.*)(\[\])(.*=.*);', r'\1\3', line)
+						line = re.sub(r'(.*)(\[\](.*);)', r'\1\3 = []', line)
+						line = re.sub(r'[A-Z]\w* (?P<vname>\w+)\s?=(?P<val>.*);', prefix+r'\g<vname> = \g<val>', line)
+						line = re.sub(r'[A-Z]\w* (?P<vname>\w+)\s?;', prefix+r'\g<vname>', line)
+				
+					line = re.sub(r'public ', '', line)
+					line = re.sub(r'private ', '', line)
+					line = re.sub(r'protected ', '', line)
+				
+					# Type erasure
+					line = re.sub(r'String (.*)', r'\1', line)
+					line = re.sub(r'\bint (\b\w*\b)', r'\1', line)
+					line = re.sub(r'Integer (.*)', r'\1', line)
+					line = re.sub(r'[Dd]ouble (.*)', r'\1', line)
+					line = re.sub(r'[Ff]loat (.*)', r'\1', line)
+					line = re.sub(r'[Ll]ong (.*)', r'\1', line)
+					line = re.sub(r'[Bb]oolean (.*)', r'\1', line)
+					
+				line = re.sub(r'new ArrayList(<\b\w*\b>)\(.*\)?', r'[]',line)
+				line = re.sub(r';', '', line)
+
+				# Control structures
+				line = re.sub(r'(\w*).equals\((\w*)\)', r'\1 == \2', line)
+				line = re.sub(r'!=\s*null', r'', line)
+				line = re.sub(r'==\s*null', r'is None', line)
+				line = re.sub(r'!\s*\(', r'not (', line)
+				line = re.sub(r'!\s*(\w*)', r'not \1', line)
 				line = re.sub(r'&&', 'and', line)
 				line = re.sub(r'\|\|', 'or', line)
+				line = re.sub(r'while\s*\((?P<cond>.*)\).*{', r'while \g<cond>: #TODO Check condition', line)
+				line = re.sub(r'else if\s*\((?P<cond>.*)\).*{', r'elif \g<cond>: #TODO Check condition', line)
+				line = re.sub(r'if\s*\((?P<cond>.*)\).*{', r'if \g<cond>: #TODO Check condition', line)
+				line = re.sub(r'}?.*else.*{?', r'else:', line)
+				line = re.sub(r'for.*\(.*(\w+)\W*=\W*(\w+)\W*;.*<\W*([\w\.\(\)\[\]]+);.*\).*\{?.*(?P<nline>\r?\n?)', r'for \1 in range(\2, \3): # FIXME \g<nline>', line)
+				line = re.sub(r'for.*\(.*(\w+)\W*=\W*(\w+)\W*;.*<=\W*([\w\.\(\)\[\]]+);.*\).*\{?.*(?P<nline>\r?\n?)', r'for \1 in range(\2, \3+1): # FIXME \g<nline>', line)
+				line = re.sub(r'for.*\(.*(\w+).*:.*(\w+).*\).*\{?.*(?P<nline>\r?\n?)', r'for \1 in \2: #TODO Check condition\g<nline> ' , line)
+
+				line = re.sub(r'\bthrow\b', r'raise', line)
 				line = re.sub(r'try\s*{', 'try:', line)
 				line = re.sub(r'catch.*\(\s*(?:final)?\s*(?P<class>\w*Exception)\s+(?P<name>\w*)\)\s*{', r'except \g<class> as \g<name>:', line)
 				line = re.sub(r'finally\s*{', 'else:' , line)
-
-				# Type erasure
-				line = re.sub(r'String (.*)', r'\1', line)
-				line = re.sub(r'int (.*)', r'\1', line)
-				line = re.sub(r'Integer (.*)', r'\1', line)
-				line = re.sub(r'[Dd]ouble (.*)', r'\1', line)
-				line = re.sub(r'[Ff]loat (.*)', r'\1', line)
-				line = re.sub(r'[Ll]ong (.*)', r'\1', line)
-				line = re.sub(r'[Bb]oolean (.*)', r'\1', line)
-
-				# Variable declaration
-				line = re.sub(r'public (.*);', r'\1', line)
-				line = re.sub(r'private (.*);', r'\1', line)
-				line = re.sub(r'protected (.*);', r'\1', line)
-				line = re.sub(r'[A-Z]\w* (?P<vname>\w+)\s?=(?P<val>.*);', r'\g<vname> = \g<val>', line)
-				line = re.sub(r';(\s*)$', '\n', line)
-
-				line = re.sub(r'\((?P<cond>.*!.*)\)', r'\(not \g<cond>):', line)
-				line = re.sub(r'\((?P<cond>.*)\).*{', r'while \g<cond>:', line)
-				line = re.sub(r'while\s*\((?P<cond>.*)\).*{', r'while \g<cond>:', line)
-				line = re.sub(r'if\s*\((?P<cond>.*)\).*{', r'if \g<cond>:', line)
-				line = re.sub(r'}?.*else.*{?', r'else:', line)
-				line = re.sub(r'for.*\(.*(\w+)\W*=\W*(\w+)\W*;.*<\W*([\w\.\(\)\[\]]+);.*\).*\{?.*(?P<nline>\r?\n?)', r'for \1 in range(\2, \3): # FIXME \g<nline>', line)
-				line = re.sub(r'for.*\(.*(\w+).*:.*(\w+).*\).*\{?.*(?P<nline>\r?\n?)', r'for \1 in \2:\g<nline>', line)
+				
+				line = re.sub(r'Integer.valueOf\((\w*)\)', r'int(\1)', line)
+				line = re.sub(r'(\w*).toString()', r'str(\1)', line)
 
 				# Method declaration
 # 				print '*', line
 # 				raw_input('')
-				match = re.search(r'([public |private |protected ])(.*?)(?P<fname>\w*)\(\) *{', line)
-				if match:
-					line = re.sub(r'([public |private |protected ])(.*?)(?P<fname>\w*)\(\) *{', r'def \g<fname>(self):', line)
-					line = '\n' + line
-
-				match = re.search(r'([public |private |protected ])(.*?)(?P<fname>\w*\()(?P<args>.*\)).+{', line)
-				if match:
-					line = re.sub(r'([public |private |protected ])(.*?)(?P<fname>\w*\()(?P<args>.*\)).+{', r'def \g<fname>self,\g<args>: ', line)
-					line = '\n' + line
 # 				print '<<', line
+				# Comments
+				line = re.sub(r'@param', ':param', line)
+				line = re.sub(r'//', '#', line)
+				line = re.sub(r'/\*|\*/', '"""', line)
 
-				if not classDefLine and className:
-					line = re.sub(className, '__init__', line)
+				# Some operator replacements
+				line = re.sub(r'(\w+)\+\+', r'\1 += 1', line)
+				line = re.sub(r'(\w+)--', r'\1 -= 1', line)
 
 				# Tokens to be completely deleted
 				line = re.sub(r'new ', '', line)
@@ -109,16 +199,9 @@ def transform(javaFile, pyFile):
 				# If no thing remains, make the line blank
 				line = re.sub(r'^\s*$', '', line)
 
-				# Some symbol replacements
-				line = re.sub(r'//', '#', line)
-				line = re.sub(r'/\*|\*/', '"""', line)
-
-				# Some operator replacements
-				line = re.sub(r'(\w+)\+\+', r'\1 \+= 1', line)
-				line = re.sub(r'(\w+)--', r'\1 -= 1', line)
 
 				# Typical line ending/closure in Java after conditions following writing conventions (ifs, fors, whiles...)
-				line = re.sub(r'\)\s*{(.*)$', r':\1', line)
+				line = re.sub(r'\)\s*{$', r':\1', line)
 
 				# Some reserved words
 				line = re.sub(r'this', 'self', line)
