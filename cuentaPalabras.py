916fa92a42191602c3d2130dde007decbe183fda
martintc <martintc@ubuntu>
2017-03-01 12:22:14 +0100

    Usada regex para detectar solo como palabra completa. Usado defaultdict para contar de forma mas corta. Cambiado el formato de cadena a python3. Refactorizado.

--- cuentaPalabras.py
+++ cuentaPalabras.py
@@ -2,11 +2,13 @@
 
 import operator
 import sys
-
-# Parametros en sys.argv
+import re
+from collections import defaultdict
 
 filename = ''
+dicc = defaultdict(int) # Si no hay clave, se utiliza int para inicializarla a 0 y recuperarla
 
+# Parametros en sys.argv
 if len(sys.argv) > 2:
 	print('Sintaxis incorrecta. Debe usar el programa del siguiente modo:')
 	print('cuentaPalabras.py [fichero] \n')
@@ -17,29 +19,19 @@ elif len(sys.argv) == 1:
 	filename = input('Introduzca el nombre exacto del fichero a procesar: ')
 else:
 	filename = sys.argv[1]
-	
-dicc = {}
 
 # with cierra el fichero
 with open(filename, 'r') as file:
-	linea = file.readline()
-	linea=linea.replace('\n', '')
+	linea = file.readline().replace('\n', '')
 	while linea != '':
-		for palabra in linea.split():
-			if palabra not in list(dicc.keys()):
-				dicc[palabra] = 1
-			else:
-				dicc[palabra] += 1
+		for palabra in re.findall(r'\b\w+\b', linea):
+			dicc[palabra] += 1
 		linea = file.readline()
 
 total = sum(dicc.values())
-
-listaOrdenada = sorted(list(dicc.items()), key=operator.itemgetter(1))
-listaOrdenada.reverse()
-
+listaOrdenada = sorted(list(dicc.items()), key=operator.itemgetter(1), reverse=True)
 
 
 with open('informe.txt', 'w') as file:
 	for (palabra,valor) in listaOrdenada:
-		file.write(palabra.ljust(15) + '%i  %2.2f %%' % (valor,(valor/float(total)*100.0))+'\n')
-
+		file.write('{0!s: <20} {1:d}  {2:2.2%}\n'.format(palabra,valor,valor/total))
