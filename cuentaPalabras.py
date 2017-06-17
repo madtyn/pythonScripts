57f9cc410e4e92edfeaee9e0c8abea52e2dd82e3
madtyn <madtyn@vant>
2017-06-17 14:17:08 +0200

    Cambiamos a ejecutar con python3 (#!/python3). Organizamos el codigo y cambiamos tabs por espacios

--- cuentaPalabras.py
+++ cuentaPalabras.py
@@ -1,36 +1,48 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import operator
 import sys
 import re
 from collections import defaultdict
 
-filename = ''
-dicc = defaultdict(int) # Si no hay clave, se utiliza int para inicializarla a 0 y recuperarla
 
-# Parametros en sys.argv
-if len(sys.argv) > 2:
-	print('Sintaxis incorrecta. Debe usar el programa del siguiente modo:')
-	print('cuentaPalabras.py [fichero] \n')
-	print('o bien\n')
-	print('python cuentaPalabras.py [fichero]\n')
-	print('(Los corchetes ([]) indican la opcionalidad de su contenido)')
-elif len(sys.argv) == 1:
-	filename = input('Introduzca el nombre exacto del fichero a procesar: ')
-else:
-	filename = sys.argv[1]
+def main():
+    '''
+    Flujo principal del programa
+    '''
+    filename = ''
+    dicc = defaultdict(int)  # Si no hay clave, se utiliza int para inicializarla a 0 y recuperarla
 
-# with cierra el fichero
-with open(filename, 'r') as fdesc:
-	linea = fdesc.readline().replace('\n', '')
-	while linea != '':
-		for palabra in re.findall(r'\b\w+\b', linea):
-			dicc[palabra] += 1
-		linea = fdesc.readline()
+    # Parametros en sys.argv
+    if len(sys.argv) > 2:
+        print('Sintaxis incorrecta. Debe usar el programa del siguiente modo:')
+        print('cuentaPalabras.py [fichero] \n')
+        print('o bien\n')
+        print('python cuentaPalabras.py [fichero]\n')
+        print('(Los corchetes ([]) indican la opcionalidad de su contenido)')
+        exit(2)
+    elif len(sys.argv) == 1:
+        filename = input('Introduzca el nombre exacto del fichero a procesar: ')
+        exit(1)
+    else:
+        filename = sys.argv[1]
 
-total = sum(dicc.values())
-listaOrdenada = sorted(list(dicc.items()), key=operator.itemgetter(1), reverse=True)
+    # with cierra el fichero
+    with open(filename, 'r') as fdesc:
+        linea = fdesc.readline().replace('\n', '')
+        while linea != '':
+            for palabra in re.findall(r'\b\w+\b', linea):
+                dicc[palabra] += 1
+            linea = fdesc.readline()
 
-with open('informe.txt', 'w') as foutput:
-	for (palabra,valor) in listaOrdenada:
-		foutput.write('{0!s: <20} {1:d}  {2:2.2%}\n'.format(palabra,valor,valor/total))
+    total = sum(dicc.values())
+    print(total)
+    listaOrdenada = sorted(list(dicc.items()), key=operator.itemgetter(1), reverse=True)
+
+    with open('informe.txt', 'w') as foutput:
+        for (palabra, valor) in listaOrdenada:
+            foutput.write('{0!s: <20} {1:d}  {2:2.2%}\n'.format(palabra, valor, valor / total))
+
+
+if __name__ == '__main__':
+    main()
